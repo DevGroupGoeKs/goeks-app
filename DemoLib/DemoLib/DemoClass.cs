cbfd3bde1e39733439cde63c08cfd4468a0d55da
test <test@csharp-developer.net>
2017-03-21 17:19:37 +0100

    TestDemoApp

---
+++ DemoLib/DemoLib/DemoClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLib
+{
+    public class DemoClass
+    {
+
+        public int GetDieAntwortAufAlleFrangen()
+        {
+            return 42;
+        }
+
+    }
+}
