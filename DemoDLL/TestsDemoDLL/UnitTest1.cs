ee5fb31fe148b7a68acdc4b6dce64082c63ae1bb
Johannes Köhler <kontakt@johanneskoehler.de>
2017-03-21 21:43:24 +0100

    Unittest gebaut
    Related Work Items: #5, #6

---
+++ DemoDLL/TestsDemoDLL/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoDLL;
+
+namespace TestsDemoDLL
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetDieRichtigeAntwortAufAllesPos()
+        {
+            var sut = new Class1();
+            var result = sut.GetDieRichtigeAntwortAufAlles();
+            Assert.AreEqual(42, result);
+        }
+
+        [TestMethod]
+        public void TestGetDieRichtigeAntwortAufAllesNeg()
+        {
+            var sut = new Class1();
+            var result = sut.GetDieRichtigeAntwortAufAlles();
+            Assert.AreNotEqual(42, result);
+        }
+    }
+}
